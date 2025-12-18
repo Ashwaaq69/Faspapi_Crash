6004d597673c795a4e2776552b282589754bd519
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2025-12-18 14:07:22 +0300

    path parametrs

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+#decorate
+@app.get('/')
+
+#function
+def index():
+    return {"data": {"Blog list"}}
+
+@app.get('/blog/{id}')
+def show(id):
+# fetch blog with id = id
+    return {"data": id}
+
+@app.get('/blog/{id}/comments')
+
+def comments(id):
+    # fetch comments of blog with id = id
+    return {"data": {"1", "2"}}    
